22abdc890438a0cb086e7253b4e8d5e3fb5f3607
EddieGilXD <djuntaied@gmail.com>
2024-05-19 20:10:30 -0600

    Se realizo el problema numero dos

---
+++ Problema II/analizador.js
@@ -0,0 +1,14 @@
+const numero = prompt("Ingresar numero a analizar");
+
+if (numero == 0) {
+    alert("El numero ingresado es igual a 0");
+} else if (numero > 0){
+    /*alert("El numero ingresado es mayor que 0");*/
+    alert("1");
+} else if(numero < 0) {
+    /*alert("El valor es meor a 0");*/
+    alert("-1");
+}
+
+/*Agregue dos alertas y las comente ya que siento que seria mejor pero las deje comentadas ya que el ejercicio no pide hacerlo*/
+
