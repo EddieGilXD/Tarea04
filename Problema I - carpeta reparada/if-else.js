77c8707c48856279b639400498366a4a1e6e83a0
EddieGilXD <djuntaied@gmail.com>
2024-05-21 00:03:07 -0600

    Se creo una copia de la carpeta para poder subirla al repositorio ya que por alguna razon la carpeta original no muestra lo que almacena

---
+++ Problema I - carpeta reparada/if-else.js
@@ -0,0 +1,7 @@
+const nombre = prompt("¿Cuál es elnombre del ‘creador’ de JavaScript?");
+
+if (nombre == "Brendan Eich") {
+    console.log("¡Correcto!")
+} else {
+    console.log("¿No lo sabes? ¡Brendan Eich!!")
+}
