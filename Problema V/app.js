7d47b98b08caaf9d32b67fbbb3948d79376139cc
EddieGilXD <djuntaied@gmail.com>
2024-05-20 21:18:50 -0600

    Se agregaron todos los archvos que estan dentro de las carpetas en el repositorio

--- Problema V/app.js
+++ Problema V/app.js
@@ -1,8 +1,13 @@
-function precioTotal(plato1, plato2, plato3) {
-    totalPlatos = plato1 + plato2 + plato3;
-    totalDeCalculo = impuesto * totalPlatos;
-    total = totalDeCalculo / 100;
-    return total;
+function precioTotal(plato1, plato2, plato3, impuesto) {
+    let precio1 = parseFloat(plato1);
+    let precio2 = parseFloat(plato2);
+    let precio3 = parseFloat(plato3);
+
+    let totalPlatos = precio1 + precio2 + precio3;
+    let totalDeCalculo = totalPlatos * impuesto;
+    let subTotal = totalDeCalculo / 100;
+    let total = parseFloat(subTotal);
+    return total.toFixed(2);
 }
 
 let plato1 = prompt("Precio del plato 1");
@@ -10,7 +15,7 @@ let plato2 = prompt("Precio del plato 2");
 let plato3 = prompt("Precio del plato 3");
 let impuesto = prompt("Agrega el porcentaje del impuesto que debe ser cobrado: %");
 
-
+let total = parseInt(precioTotal(plato1, plato2, plato3, impuesto))
 
 alert("El total final seria de: " + total)
 
